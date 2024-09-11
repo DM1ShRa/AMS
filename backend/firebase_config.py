d597ff3b42446756f009ea1c13000f9625b83000
DM1ShRa <darash.mishra@gmail.com>
2024-09-11 16:26:32 +0530

    Revert "svix-webhooks for clerk with firebase"
    This reverts commit dfdeddb6f28d9ed96913fdffcfa2da42df463890.
    
    CLERK AUTH Optimized

--- backend/firebase_config.py
+++
@@ -1,7 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
-
-cred = credentials.Certificate("amsproj-f7dbd-firebase-adminsdk-fuesr-dd04cce976.json")
-firebase_admin.initialize_app(cred)
-
-db = firestore.client()
