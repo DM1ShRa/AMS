051ccb296bb27c8a96a909a8b0bba4978b532643
DM1ShRa <darash.mishra@gmail.com>
2024-10-20 22:52:29 +0530

    FIREBASE CONFIG

--- frontend/src/Firebase/firebaseConfig.js
+++ frontend/src/Firebase/firebaseConfig.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  databaseURL: process.env.databaseURL,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
+  apiKey: process.env.VITE_API_KEY,
+  authDomain: process.env.VITE_AUTH_DOMAIN,
+  databaseURL: process.env.VITE_DATABASE_URL,
+  projectId: process.env.VITE_PROJECT_ID,
+  storageBucket: process.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
