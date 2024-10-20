1ec81a3d9e208382dc0f0e880f403baa5e1333dd
DM1ShRa <darash.mishra@gmail.com>
2024-10-20 23:28:14 +0530

    Backend for deployment

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,21 @@ from sklearn.preprocessing import StandardScaler
 
 load_dotenv()
 
-cred = credentials.Certificate(os.getenv('FIREBASE_CREDENTIALS_PATH'))
+firebase_creds = {
+    "type": "service_account",
+    "project_id": os.getenv('project_id'),
+    "private_key_id": os.getenv('private_key_id'),
+    "private_key": os.getenv('private_key').replace('\\n', '\n'),
+    "client_email": os.getenv('client_email'),
+    "client_id": os.getenv('client_id'),
+    "auth_uri": os.getenv('auth_uri'),
+    "token_uri": os.getenv('token_uri'),
+    "auth_provider_x509_cert_url": os.getenv('auth_provider_x509_cert_url'),
+    "client_x509_cert_url": os.getenv('client_x509_cert_url'),
+    "universe_domain": os.getenv('universe_domain')
+}
+
+cred = credentials.Certificate(firebase_creds)
 firebase_admin.initialize_app(cred, {
     'databaseURL': os.getenv('FIREBASE_DATABASE_URL')
 })
